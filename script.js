5fb1b125a8943d72dd7d5aca92059b818895053c
chitrafore <chi.nguyen@trafore.fi>
2022-06-02 16:07:34 +0300

    style update

--- script.js
+++ script.js
@@ -10,3 +10,18 @@ document.querySelectorAll(".nav-links").forEach( button => button. addEventListe
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }))
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
