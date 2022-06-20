e02c1ce58639c448ac92b86309dba8c442a422fe
Chi Nguyen <nglinhchi65@gmail.com>
2022-06-20 12:00:22 +0300

    fade-in effect

--- script.js
+++ script.js
@@ -78,3 +78,21 @@ closeButtons.forEach(close => {
         toggle.focus(); // Return keyboard focus to "toggle" button.
     });
 });
+
+
+//fade-in effect 
+let elementsArray = document.querySelectorAll(".module-grid, .introduction, .statistics, #happy-ending, .module-tx");
+console.log(elementsArray);
+window.addEventListener('scroll', fadeIn ); 
+function fadeIn() {
+    for (var i = 0; i < elementsArray.length; i++) {
+        var elem = elementsArray[i]
+        var distInView = elem.getBoundingClientRect().top - window.innerHeight + 20;
+        if (distInView < 0) {
+            elem.classList.add("inView");
+        } else {
+            elem.classList.remove("inView");
+        }
+    }
+}
+fadeIn();
